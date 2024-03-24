4727f77b6faaa361f69825f82aca088389775253
Kim Seongjin <darkmoon625@naver.com>
2024-03-24 15:45:18 +0900

    Revert "Revert "Initial Setup""
    This reverts commit 750cb8db072b22117f420287b112767ecc53f252.

---
+++ src/test/java/site/foolish/ary/AryApplicationTests.java
@@ -0,0 +1,13 @@
+package site.foolish.ary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
