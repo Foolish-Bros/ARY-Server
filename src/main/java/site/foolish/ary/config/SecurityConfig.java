0f46337d9421e21009f42b0016d82655992bc09e
Kim Seongjin <darkmoon625@naver.com>
2024-05-20 10:11:16 +0900

    Set cors config

--- src/main/java/site/foolish/ary/config/SecurityConfig.java
+++ src/main/java/site/foolish/ary/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import site.foolish.ary.domain.member.Role;
 import site.foolish.ary.filter.member.JWTFilter;
 import site.foolish.ary.filter.member.LoginFilter;
@@ -98,7 +99,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 //        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://www.all-review-young.site"));
 //        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 //        source.registerCorsConfiguration("/**", configuration);
@@ -112,12 +113,9 @@ public class SecurityConfig {
         return source;
     }
 
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring()
-//                .requestMatchers("/member/**")
-//                .requestMatchers("/")
-//                .requestMatchers("/resources/**");
-//    }
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
 }
 
